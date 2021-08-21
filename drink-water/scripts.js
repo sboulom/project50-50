f9f8b0728640a4647f9a192a7b044ee710102e82
sboulom <sboulom@gmail.com>
2021-08-21 08:03:37 -0700

    big cup function

--- drink-water/scripts.js
+++ drink-water/scripts.js
@@ -3,20 +3,25 @@ const listers = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
-smallCups.forEach((cup, idx)=>{
+updateBigCup()
+
+smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 })
 
 function highlightCups(idx) {
-    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+    if (idx===7 && smallCups[idx].classList.contains("full")) idx--;
+    else if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
         idx--
     }
 
     smallCups.forEach((cup, idx2) => {
-        if(idx2 <= idx){
+        if(idx2 <= idx) {
             cup.classList.add('full')
         } else {
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
 }
