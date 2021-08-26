66a722d742b953ee61e5169b4b29184e3e9dbe68
sboulom <sboulom@gmail.com>
2021-08-26 07:41:32 -0700

    async funciton for api

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,6 +1,6 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=27aa767a2b081310417970e5ce547249&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=27aa767a2b081310417970e5ce547249&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
@@ -15,3 +15,17 @@ async function getMovies(url) {
 
     showMovies(data.results)
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
